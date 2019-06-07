f8a5d2e1873b4670b3445cec82b4355271af309e
Matheus Albino <matheusjva@gmail.com>
2019-06-06 22:06:33 -0300

    Criado as migrations para as tabelas Vagas e Candidatos.

---
+++ database/migrations/2019_06_07_005548_create_candidatos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCandidatosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidatos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('vaga_id');
+            $table->string('nome');
+            $table->string('email');
+            $table->string('escolaridade');
+            $table->string('curriculo');
+            $table->timestamps();
+
+            $table->foreign('vaga_id')->references('id')->on('vagas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidatos');
+    }
+}
