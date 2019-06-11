57cf7e8eb0e7a8f6a9d487ba41c057ca896b89ed
Matheus Albino <matheusjva@gmail.com>
2019-06-11 20:27:53 -0300

    Modificação no controller home para exibir os candidatos na view home.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Candidatos;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $candidatos = DB::table('candidatos')->get()->all();
+
+        return view('home', compact('candidatos'));
     }
 }
