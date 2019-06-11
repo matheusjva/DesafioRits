eb3b9e4bfb735aff42a1601da39bb3d12eff4df5
Matheus Albino <matheusjva@gmail.com>
2019-06-11 20:31:42 -0300

    Adicionado rota para fazer download do arquivo pdf/doc.

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@ Route::resource('candidatos', 'CandidatosController');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/download/{filename}', 'CandidatosController@download');
